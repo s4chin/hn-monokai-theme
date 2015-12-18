0e391fccb08a5601b1b391d00f91cbf784d002cf
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-18 21:34:03 +0530

    Add userstyle

---
+++ userstyles.js
@@ -0,0 +1,48 @@
+@-moz-document domain("news.ycombinator.com") {
+* {
+    color: #dcdccc !important;
+    background-color: #272822 !important;
+    font-family: "Lucida Console", verdana !important;
+}
+body > center > table > tbody > tr:first-child * {
+    background-color: #75715E !important;
+    /*font-family: verdana, helvetica !important;
+    font-size: 10pt !important;*/
+}
+.pagetop a {
+    color: #AE81FF !important;
+}
+.brandname a {
+    color: #66D9EF !important;
+}
+body > center > table > tbody > tr > td > table > tbody > tr > td:last-child .pagetop a {
+    color: #F92672 !important;
+    /*font-family: verdana, helvetica !important;
+    font-size: 10pt !important;*/
+}
+a {
+    color: #F8F8F2 !important;
+}
+.athing a:visited {
+    color: #74705D !important;
+}
+.athing a:hover {
+    color: #C1C1C1 !important;
+}
+.subtext a {
+    color: #E6DB74 !important;
+}
+.score {
+    color: #FD971F !important;
+}
+.sitebit,
+.sitestr {
+    color: #A6E22E !important;
+}
+.votelinks a {
+    color: #111111 !important;
+}
+.yclinks a {
+    color: #66D9EF !important;
+}
+}
